300f44ae93ad8879b6ecff7d9020ce6eb863698d
DNAlchemist <DNAlchemist@ya.ru>
2019-11-20 13:35:08 +0300

    Add debug flag to update action

--- scripts/liquibase_update.sh
+++ scripts/liquibase_update.sh
@@ -2,4 +2,4 @@
 : ${CHANGELOG_FILE:="changelogs.xml"}
 
 echo "Applying changes to the database. Changelog: $CHANGELOG_FILE"
-liquibase --changeLogFile="$CHANGELOG_FILE" update
+liquibase --logLevel="debug" --changeLogFile="$CHANGELOG_FILE" update
